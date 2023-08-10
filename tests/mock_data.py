52a3b6c24a5e22d4ead6a73955801f41249545d3
micaelmz <contato@micaelmuniz.com>
2023-08-10 02:53:48 -0300

    🧪 Aprimorando testes dos modelos para compatibilidade com o SQLAlchemy, integrando o uso do flask_testing para emular o contexto utilizado pelo Flask SQLAlchemy.

--- tests/mock_data.py
+++ tests/mock_data.py
@@ -1,43 +1,78 @@
 from models.product import Product
 from models.review import Review
-from database import Database
-import json
+from models.category import Category
+from models.user import User
+from datetime import datetime
+
+test_users = [
+    User(
+        name="User1"
+    ),
+    User(
+        name="User2"
+    ),
+    User(
+        name="User3"
+    )
+]
+
+test_categories = [
+    Category(
+        name="Test Category",
+        image_url="https://www.test.com/test.jpg"
+    ),
+    Category(
+        name="Test Category 2",
+        image_url="https://www.test.com/test2.jpg"
+    )
+]
 
 test_reviews = [
-    Review(id=1, author_id=1, product_id=1, content="Good",
-           grade=4.0, date="01-01-2021", author_name="User1"
-           ),
-    Review(id=2, author_id=2, product_id=1, content="Excellent",
-           grade=4.8, date="01-01-2021", author_name="User2"
-           ),
-    Review(id=3, author_id=3, product_id=1, content="Average",
-           grade=3.5, date="01-01-2021", author_name="User3"
-           )
+    Review(
+        author_id=1,
+        product_id=1,
+        content="Good",
+        review_grade=4.0,
+        date=datetime.now()
+    ),
+    Review(
+        author_id=2,
+        product_id=1,
+        content="Excellent",
+        review_grade=4.8,
+        date=datetime.now()
+    ),
+    Review(
+        author_id=3,
+        product_id=1,
+        content="Average",
+        review_grade=3.5,
+        date=datetime.now()
+    )
 ]
 test_products = [
     Product(
-        id=1,
         name="Test Product",
-        price=25.99,
+        price_current=25.99,
         price_old=50.0,
-        category='Test Category',
-        promotion=False,
-        image_url='https://www.test.com/test.jpg',
+        category=1,
+        promotion=True,
+        image_thumb='https://www.test.com/test.jpg',
         description='Test Description',
-        reviews=test_reviews,
-        additional_info=json.dumps({'Test Info': 'Test Value'}, ensure_ascii=False),
-        extra_images=json.dumps({'img1': 'https://www.test.com/test1.jpg'}, ensure_ascii=False)
+        color='Test Color',
+        additional_info={'Test Info': 'Test Value'},
+        extra_images={'img1': 'https://www.test.com/test1.jpg'}
     ),
     Product(
-        id=2,
-        name="Test Product 1",
-        price=16.99,
-        price_old=45.0,
-        category='Test Category',
-        promotion=True,
-        image_url='https://www.test.com/test2.jpg',
-        description='Test Description 2',
+        name="Test Product 2",
+        price_current=18.0,
+        price_old=0,
+        category=2,
+        promotion=False,
+        image_thumb='https://www.test.com/test.jpg',
+        description='Another Test Description',
+        color='Test Color 2',
+        additional_info={'Second Test Info': 'Second Test Value'},
+        extra_images={'img1': 'https://www.test.com/test1.jpg'}
     )
 ]
-db = Database('tests/mock.db')
-db.cursor.executescript(open('tests/mock.sql').read())
