bfbfb4bdc65e1e74e597f4b241cf5d1e4a080d09
micaelmz <contato@micaelmuniz.com>
2023-08-18 15:15:35 -0300

    🚀 Implementação de routes para product, user e no_result

---
+++ routes/product_bp.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+from controllers.product_controller import all_products, products_by_category, search_page, search_form, search_products
+
+product_blueprint = Blueprint('product', __name__)
+
+product_blueprint.route('/all', methods=['GET'])(all_products)
+product_blueprint.route('/category/<int:category_id>', methods=['GET'])(products_by_category)
+product_blueprint.route('/search', methods=['GET'])(search_page)
+product_blueprint.route('/search', methods=['POST'])(search_form)
+product_blueprint.route('/search/<string:search_query>', methods=['GET'])(search_products)
