a194a4b13c5d0301ade07301448ac4ce00550b9e
micael <micaelmuniz6@gmail.com>
2023-08-20 15:03:59 -0300

    🚀 Dando início à implementação de autenticação de usuários, registro e login!

--- config.py
+++ config.py
@@ -1,6 +1,17 @@
 import os
+from flask import Flask
+from flask_login import LoginManager
+from utils.database import db
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-DEBUG = True
-SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['basedir'] = os.path.abspath(os.path.dirname(__file__))
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+db.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'user.login'
+
+with app.app_context():
+    db.create_all()
--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,6 +1,11 @@
 from flask import render_template
 from models.category import Category
 from models.user import User
+from utils.security import calculate_salting_length
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from wtforms import StringField
+from wtforms.validators import DataRequired, Email, Length
 
 
 def login():
@@ -15,15 +20,15 @@ def register():
     return render_template('register.html')
 
 
+def register_post():
+    pass
+
+
 def register_validation():
     return render_template('validation.html')
 
 
 def register_validation_post():
-    pass
-
-
-def register_post(user: User):
     pass
 
 
