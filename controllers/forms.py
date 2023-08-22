7fc9612552471d626470fccd1a1a0635a86e3daa
micaelmz <contato@micaelmuniz.com>
2023-08-22 00:19:06 -0300

    🚀 Concluindo a implementação da autenticação de usuários, incluindo as funcionalidades de login e cadastro.

---
+++ controllers/forms.py
@@ -0,0 +1,72 @@
+from flask_wtf import FlaskForm
+from flask import flash, redirect, url_for
+from wtforms import StringField, PasswordField, validators
+from wtforms.validators import DataRequired, Email, EqualTo
+
+
+def validate_form_or_back(form: FlaskForm):
+    """
+    Validates a form and redirects to the previous page with flash messages if it's invalid
+    :param form: Form to be validated
+    :return:
+    """
+    if not form.validate_on_submit():
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash(error, 'warning')
+        return redirect(url_for('user.pre_login'))
+    return True
+
+
+class PreLoginForm(FlaskForm):
+    """
+    Form used to validate the email before login or registration
+    """
+    email = StringField(
+        'Email',
+        validators=[
+            DataRequired(message='O campo email é obrigatório.'),
+            Email(message='O email informado não é válido.')
+        ]
+    )
+
+
+class LoginForm(PreLoginForm):
+    """
+    Form used to validate the email and password before login
+    """
+    password = PasswordField(
+        'Senha',
+        validators=[
+            DataRequired(message='O campo senha é obrigatório.')
+        ]
+    )
+
+
+class RegistrationForm(FlaskForm):
+    """
+    Form used to validate the email, first name, last name and password before registration
+    """
+    email = StringField(
+        'Email', validators=[
+            DataRequired(message='O campo email é obrigatório.'),
+            Email(message='O email informado não é válido.')
+        ])
+    first_name = StringField(
+        'Nome', validators=[
+            DataRequired(message='O campo nome é obrigatório.')
+        ])
+    last_name = StringField(
+        'Sobrenome', validators=[
+            DataRequired(message='O campo sobrenome é obrigatório.')
+        ])
+    password = PasswordField(
+        'Senha', validators=[
+            DataRequired(message='O campo senha é obrigatório.'),
+            validators.Length(min=8, message="A senha deve ter no mínimo 8 caracteres"),
+            EqualTo('confirm_password', message='As senhas devem ser iguais')
+        ])
+    confirm_password = PasswordField(
+        'Confirm Password', validators=[
+            DataRequired(message='O campo confirmar senha é obrigatório.')
+        ])
--- routes/user_bp.py
+++ routes/user_bp.py
@@ -1,12 +1,12 @@
 from flask import Blueprint
-from controllers.user_controller import login, login_post, register, register_post, register_validation, \
-    register_validation_post, cart, logout
+from controllers.user_controller import pre_login, pre_login_post, register_post, register_validation, \
+    register_validation_post, cart, logout, login_post
 
 user_blueprint = Blueprint('user', __name__)
 
-user_blueprint.route('/login', methods=['GET'])(login)
-user_blueprint.route('/login', methods=['POST'])(login_post)
-user_blueprint.route('/register', methods=['GET'])(register)
+user_blueprint.route('/login', methods=['GET'])(pre_login)
+user_blueprint.route('/login', methods=['POST'])(pre_login_post)
+user_blueprint.route('/login/continue', methods=['POST'])(login_post)
 user_blueprint.route('/register', methods=['POST'])(register_post)
 user_blueprint.route('/register/validation', methods=['GET'])(register_validation)
 user_blueprint.route('/register/validation', methods=['POST'])(register_validation_post)
