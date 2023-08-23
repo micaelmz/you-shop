69c4d10d280f776034f58d48c4132b3b7ebabfa4
micaelmz <contato@micaelmuniz.com>
2023-08-23 20:15:28 -0300

    🚀 WSGI adicionado para fins de deployment.

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = os.getenv('SECRET_KEY_FLASK')
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///database.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-DEBUG = False
+DEBUG = os.getenv('DEBUG', False)
