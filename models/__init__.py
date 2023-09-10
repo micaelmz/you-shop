a5d74a4c0e53f2327b59e491a535f496c407c8c0
Micael <micaelmuniz6@gmail.com>
2023-09-10 02:45:52 -0300

    🛠 Refatorando e organizando o código para melhor compreensão

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from .cart import Cart
+from .category import Category
+from .product import Product, Price
+from .review import Review, Rating
+from .user import User
---
+++ routes/__init__.py
@@ -0,0 +1,4 @@
+from .auth_bp import auth_blueprint
+from .no_results_bp import no_results_blueprint
+from .product_bp import product_blueprint
+from .user_bp import user_blueprint
